3ffe083a279bd721582e60a46b69617a8e5b995f
otis-tran <otis.tran2000@gmail.com>
2025-03-13 23:08:03 +0700

    [02] Complete exercise

--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -1,7 +1,11 @@
 // TODO: Fix the function body without changing the signature.
 fn square(num: i32) -> i32 {
-    num * num;
+    return num * num;
 }
+
+// fn square(num: i32) -> i32 {
+//     num * num
+// }
 
 fn main() {
     let answer = square(3);
